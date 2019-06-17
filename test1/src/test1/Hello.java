9d888f02ca35246fa34c8c44f769379a5191d3f3
jiya <openmind011@nate.com>
2019-06-17 16:46:37 +0900

    Tom 추가

--- test1/src/test1/Hello.java
+++ test1/src/test1/Hello.java
@@ -2,7 +2,8 @@ package test1;
 
 public class Hello {
 	public static void main(String [] args) {
-		System.out.println("¾È³ç");
+		System.out.println("Hello");
 		System.out.println("Wold");
+		System.out.println("He's name is Tom");
 	}
 }
